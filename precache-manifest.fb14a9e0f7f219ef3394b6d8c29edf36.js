9e24bc6a97342bd41235a9b2a71edd8b511be51e
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-06-14 17:28:20 +0300

    Updates

---
+++ precache-manifest.fb14a9e0f7f219ef3394b6d8c29edf36.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf3eccb71c78194621955acb50f8c85",
+    "url": "/decision/index.html"
+  },
+  {
+    "revision": "b83a0f145a8a47bc71bd",
+    "url": "/decision/static/css/main.670bf8e9.chunk.css"
+  },
+  {
+    "revision": "e209f7ddaaf5178db3b8",
+    "url": "/decision/static/js/2.d53cde9a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/decision/static/js/2.d53cde9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83a0f145a8a47bc71bd",
+    "url": "/decision/static/js/main.a04c9bfb.chunk.js"
+  },
+  {
+    "revision": "979b9121f7a868d9c4dd",
+    "url": "/decision/static/js/runtime-main.85113173.js"
+  },
+  {
+    "revision": "e85f2b61a1faeb789a4a4db88314c699",
+    "url": "/decision/static/media/diagram.e85f2b61.svg"
+  },
+  {
+    "revision": "18410bcefa18ddf706ee4e5d8ca8170d",
+    "url": "/decision/static/media/logo.18410bce.svg"
+  },
+  {
+    "revision": "ce760af511d200abee8e26c9c7c9ed33",
+    "url": "/decision/static/media/sprite.ce760af5.svg"
+  }
+]);
